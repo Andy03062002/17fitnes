245e47adcc127aa1bcfa71e969193252db6b38aa
Andy03062002 <christyan.moya@ist17dejulio.edu.ec>
2025-11-08 22:31:37 -0500

    login y registro con adminlte

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Panel Principal')
+
+@section('content_header')
+    <h1>Bienvenido a 17Fitness</h1>
+@stop
+
+@section('content')
+    <p>Has iniciado sesión correctamente.</p>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
